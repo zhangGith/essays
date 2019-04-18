a749a7d230f3f894df8df4b842d38a1be2dc4fc2
block_zhang <zhangchuanchuan@donview.cn>
2019-04-18 11:11:42 +0800

    first commit

---
+++ DssaysDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DssaysDemo
+//
+//  Created by Block on 2019/4/18.
+//  Copyright © 2019年 Block. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
